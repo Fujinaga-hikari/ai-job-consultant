6bfa9f2cbeaa62185890fbadb2a3d267546dd234
Fujinaga-hikari <ryo_fujinaga@po.hikari.co.jp>
2026-05-26 00:43:21 +0900

    Fix window.gtag not defined for custom event tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,9 +67,9 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         />
         <Script id="ga-init" strategy="afterInteractive">{`
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-E3T47NQM6M');
+          window.gtag = function(){window.dataLayer.push(arguments);}
+          window.gtag('js', new Date());
+          window.gtag('config', 'G-E3T47NQM6M');
         `}</Script>
       </body>
     </html>
